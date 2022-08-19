c9507e265ff9496dbc79efc00b76d8ea8d381b4f
BolongZhang-AWS <88056513+BolongZhang-AWS@users.noreply.github.com>
2022-08-19 14:05:36 -0700

    fix: add retry with jitter when client initialization failed (#10)

--- greengrass_defender_agent/agent.py
+++ greengrass_defender_agent/agent.py
@@ -2,7 +2,9 @@
 # SPDX-License-Identifier: Apache-2.0
 
 from threading import Timer, Thread
-
+from random import randint
+from time import sleep
+from os import environ
 from AWSIoTDeviceDefenderAgentSDK import collector
 from greengrass_defender_agent import config
 from greengrass_defender_agent import ipc_utils
@@ -43,6 +45,45 @@ def set_configuration(configuration):
         )
 
     return new_config
+
+
+def set_env_variables_config():
+    env_config = {}
+    if environ.get(config.PUBLISH_RETRY_CONFIG_KEY):
+        try:
+            connect_publish_retry = int(environ.get(config.PUBLISH_RETRY_CONFIG_KEY))
+        except (ValueError, TypeError):
+            config.logger.warning(
+                "Invalid retry time. Using default retry count: {}".format(
+                    config.DEFAULT_CONNECT_AND_PUBLISH_RETRY
+                )
+            )
+            connect_publish_retry = config.DEFAULT_CONNECT_AND_PUBLISH_RETRY
+        if connect_publish_retry < config.MIN_PUBLISH_RETRY:
+            config.logger.warning(
+                "Using minimum retry count: {}".format(
+                    config.MIN_PUBLISH_RETRY
+                )
+            )
+            env_config[config.PUBLISH_RETRY_CONFIG_KEY] = config.MIN_PUBLISH_RETRY
+        elif connect_publish_retry > config.MAX_PUBLISH_RETRY:
+            config.logger.warning(
+                "Using maximum retry count: {}".format(
+                    config.MAX_PUBLISH_RETRY
+                )
+            )
+            env_config[config.PUBLISH_RETRY_CONFIG_KEY] = config.MAX_PUBLISH_RETRY
+        else:
+            env_config[config.PUBLISH_RETRY_CONFIG_KEY] = connect_publish_retry
+    else:
+        env_config[config.PUBLISH_RETRY_CONFIG_KEY] = config.DEFAULT_CONNECT_AND_PUBLISH_RETRY
+        config.logger.warning(
+            "Using default retry count: {}".format(
+                config.DEFAULT_CONNECT_AND_PUBLISH_RETRY
+            )
+        )
+
+    return env_config
 
 
 def set_configuration_and_publish(ipc_client, configuration, metrics_collector):
@@ -103,16 +144,46 @@ def publish_metrics(ipc_client, config_changed, metrics_collector, sample_interv
         raise e
 
 
+def handle_expcetion_and_sleep(retry_action, exception, retry_time_sec, retry_count):
+    """
+    Handle exception and sleep for retry
+
+    :param retry_action: the name of retry action to print in the log
+    :param exception: the exception thrown from orginal action
+    :param retry_time_sec: number of seconds to sleep before the next retry
+    """
+    config.logger.exception(
+                "Exception occurred during the {} : {}".format(retry_action, exception)
+                )
+    config.logger.info(
+                "Will retry {} in {} seconds, retry count remaining: {}".format(retry_action, retry_time_sec, retry_count)
+            )
+    sleep(retry_time_sec)
+
+
 def main():
     # Get the ipc client
     ipc_client = ipc_utils.IPCUtils()
-    try:
-        ipc_client.connect()
-    except Exception as e:
-        config.logger.error(
-            "Exception occurred during the creation of an IPC client: {}".format(e)
-        )
-        exit(1)
+
+    # Connect to the GG Nucleus
+    need_retry = True
+    retry_time = config.INITIAL_RETRY_INTERVAL_SECONDS
+    connect_retry = config.DEFAULT_CONNECT_AND_PUBLISH_RETRY
+
+    while (need_retry and connect_retry > 0):
+        try:
+            ipc_client.connect()
+            need_retry = False
+        except Exception as e:
+            handle_expcetion_and_sleep("IPC client initialization", e, retry_time, connect_retry)
+            if retry_time < config.MAX_RETRY_INTERVAL_SECONDS:
+                retry_time = retry_time * 2 + randint(0, config.MAX_JITTER_TIME_INTERVAL_SECONDS)
+            else:
+                retry_time = config.MAX_RETRY_INTERVAL_SECONDS
+
+            if connect_retry < 1:
+                exit(1)
+        connect_retry -= 1
 
     # Get initial configuration from the recipe
     configuration = ipc_client.get_configuration()
@@ -125,7 +196,22 @@ def main():
     metrics_collector = collector.Collector(short_metrics_names=False)
 
     # Start collecting and publishing metrics
-    set_configuration_and_publish(ipc_client, configuration, metrics_collector)
+    env_config = set_env_variables_config()
+    publish_retry = env_config[config.PUBLISH_RETRY_CONFIG_KEY]
+    need_retry = True
+    retry_time = config.INITIAL_RETRY_INTERVAL_SECONDS
+
+    while (need_retry and publish_retry >= 0):
+        try:
+            set_configuration_and_publish(ipc_client, configuration, metrics_collector)
+            need_retry = False
+        except Exception as e:
+            handle_expcetion_and_sleep("metrics publish", e, retry_time, publish_retry)
+            if retry_time < config.MAX_RETRY_INTERVAL_SECONDS:
+                retry_time = retry_time * 2 + randint(0, config.MAX_JITTER_TIME_INTERVAL_SECONDS)
+            else:
+                retry_time = config.MAX_RETRY_INTERVAL_SECONDS
+        publish_retry -= 1
 
     # Subscribe to the subsequent configuration changes
     ipc_client.subscribe_to_config_updates()
